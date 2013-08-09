261ea81a6ae1cc8d9e006aae75e9eabb128d80e4
gidema <mail@gertjanidema.nl>
2013-08-09 09:33:03 +0200

    Start of 0.2.4 development.

---
+++ src/main/java/org/openstreetmap/josm/plugins/openservices/buildings/package.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides tools to compare ODS and OSM layers
+ * with respect to buildings and addresses.
+ * 
+ */
+package org.openstreetmap.josm.plugins.openservices.buildings;
