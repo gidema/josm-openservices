55be1ad74751e5830625d781fa3932081f975944
gidema <mail@gertjanidema.nl>
2017-03-07 19:19:51 +0100

    Fixed ods-bag #20

--- src/main/java/org/openstreetmap/josm/plugins/ods/DiscardOdsTagsHook.java
+++ src/main/java/org/openstreetmap/josm/plugins/ods/DiscardOdsTagsHook.java
@@ -3,8 +3,10 @@ package org.openstreetmap.josm.plugins.ods;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.upload.UploadHook;
@@ -14,22 +16,33 @@ import org.openstreetmap.josm.data.APIDataSet;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 
 /**
- * Removes all ODS tags from all modified objects before upload
- */
+ * Removes all ODS tags from all modified objects before upload.
+  */
 class DiscardOdsTagsHook implements UploadHook {
 
     @Override
     public final boolean checkUpload(APIDataSet apiDataSet) {
         List<OsmPrimitive> objectsToUpload = apiDataSet.getPrimitives();
-
-        Map<String, String> map = new HashMap<>();
-        for (String key : ODS.KEYS) {
-            map.put(key, null);
+        
+        // Collect distinct ODS keys in the data set
+        Set<String> odsKeys = new HashSet<>();
+        for (OsmPrimitive osm : objectsToUpload) {
+            for (String key : osm.keySet()) {
+                if (key.startsWith(ODS.KEY.BASE)) {
+                    odsKeys.add(key);
+                }
+            }
         }
 
-        SequenceCommand removeKeys = new SequenceCommand(tr("Removed ODS tags"),
+        if (!odsKeys.isEmpty()) {
+            Map<String, String> map = new HashMap<>();
+            for (String key : odsKeys) {
+                map.put(key, null);
+            }
+            SequenceCommand removeKeys = new SequenceCommand(tr("Removed ODS tags"),
                 new ChangePropertyCommand(objectsToUpload, map));
-        Main.main.undoRedo.add(removeKeys);
+            Main.main.undoRedo.add(removeKeys);
+        }
         return true;
     }
 }
