9fd39c38189036466d760f3fbe9c5219df807779
gidema <mail@gertjanidema.nl>
2017-03-15 12:16:08 +0100

    Fixed #51 Warn about pending changes on reset

--- src/main/java/org/openstreetmap/josm/plugins/ods/gui/OdsResetAction.java
+++ src/main/java/org/openstreetmap/josm/plugins/ods/gui/OdsResetAction.java
@@ -2,7 +2,11 @@ package org.openstreetmap.josm.plugins.ods.gui;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.JOptionPane;
+
+import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.plugins.ods.OdsModule;
+import org.openstreetmap.josm.tools.I18n;
 
 public class OdsResetAction extends OdsAction {
 
@@ -18,6 +22,18 @@ public class OdsResetAction extends OdsAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        module.reset();
+        boolean reset = true;
+        if (module.getOsmLayerManager().getOsmDataLayer().requiresUploadToServer()) {
+            String title = I18n.tr("Are you sure?");
+            String message = I18n.tr(
+                "There are unsaved changes on the OSM layer.\n" +
+                "Are you sure you want to reset and lose these changes?");
+            int answer = JOptionPane.showOptionDialog(Main.panel, message, title, JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.WARNING_MESSAGE, null, null, JOptionPane.CANCEL_OPTION);
+            reset = (answer == JOptionPane.OK_OPTION);
+        }
+        if (reset) {
+            module.reset();
+        }
     }
 }
