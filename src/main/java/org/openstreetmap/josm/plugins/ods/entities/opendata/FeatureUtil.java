bd636ca354678e6b07534566f81874a7d2d559e3
gidema <email@gertjanidema.nl>
2020-01-01 15:02:12 +0100

    Interpret WFS empty string as null
    The MapServer WFS implementation returns null values as empty xml
    elements rather than leaving these elements out.
    This results in empty strings and conversion exceptions in the
    FeatureUtils class. This patch lets FeatureUtils treat empty elements as
    null.

--- src/main/java/org/openstreetmap/josm/plugins/ods/entities/opendata/FeatureUtil.java
+++ src/main/java/org/openstreetmap/josm/plugins/ods/entities/opendata/FeatureUtil.java
@@ -1,6 +1,8 @@
 package org.openstreetmap.josm.plugins.ods.entities.opendata;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Optional;
 
 import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
@@ -22,13 +24,19 @@ public class FeatureUtil {
 
     public static BigDecimal getBigDecimal(SimpleFeature feature, String name ) {
         Property property = feature.getProperty(name);
-        if (property == null) return null;
+        if (property == null || property.getValue() == null) return null;
         return (BigDecimal) property.getValue();
+    }
+
+    public static BigInteger getBigInteger(SimpleFeature feature, String name) {
+        Property property = feature.getProperty(name);
+        if (property == null || property.getValue() == null) return null;
+        return (BigInteger) property.getValue();
     }
 
     public static String getString(SimpleFeature feature, String name ) {
         Property property = feature.getProperty(name);
-        if (property == null) return null;
+        if (property == null || property.getValue() == null) return null;
         return (String) property.getValue();
     }
     
@@ -37,19 +45,23 @@ public class FeatureUtil {
         return (s == null || s.isEmpty()) ? null : s.charAt(0);
     }
     
+    public static Optional<String> getOptionalString(SimpleFeature feature, String name ) {
+        Property property = feature.getProperty(name);
+        if (property == null  || property.getValue() == null) return Optional.empty();
+        return Optional.of((String) property.getValue());
+    }
+    
     public static Integer getInteger(SimpleFeature feature, String name ) {
         Property property = feature.getProperty(name);
-        if (property == null) return null;
+        if (property == null || property.getValue() == null) return null;
         Number number = (Number) property.getValue();
-        if (number == null) return null;
         return number.intValue();
     }
     
     public static Long getLong(SimpleFeature feature, String name ) {
         Property property = feature.getProperty(name);
-        if (property == null) return null;
+        if (property == null || property.getValue() == null) return null;
         Number number = (Number) property.getValue();
-        if (number == null) return null;
         return number.longValue();
     }
     
