6e7dcd36438ccc32c3b6f156b666f026813b32ee
gidema <email@gertjanidema.nl>
2020-01-03 20:45:25 +0100

    Fixed issue ods-bag issue 31

--- src/main/java/org/openstreetmap/josm/plugins/ods/domains/buildings/BuildingType.java
+++ src/main/java/org/openstreetmap/josm/plugins/ods/domains/buildings/BuildingType.java
@@ -3,16 +3,4 @@ package org.openstreetmap.josm.plugins.ods.domains.buildings;
 public enum BuildingType {
     UNCLASSIFIED, HOUSE, HOUSEBOAT, STATIC_CARAVAN, INDUSTRIAL, 
     RETAIL, OFFICE, APARTMENTS, GARAGE, SUBSTATION, PRISON, OTHER;
-    
-    private String subType;
-
-    public String getSubType() {
-        return subType;
-    }
-
-    public static BuildingType OTHER(String subType) {
-        BuildingType other = BuildingType.OTHER;
-        other.subType = subType;
-        return other;
-    }
 }
